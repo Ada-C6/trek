326598665744b34b4617b9908bf6a4aea4e7ea45
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-11-22 14:46:06 -0800

    set up to read in api and display trips with links

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
+  // 'get' URL
+  var url = 'https://trektravel.herokuapp.com/trips';
+
+  // Response
+  var successCallback = function(response) {
+    console.log('sucess!');
+    console.log(response);
+
+    var body = $('.travel-body');
+    body.empty();
+
+    $.each(response, function(index, trip) {
+      var row = $('<tr></tr>');
+      var name = $('<td><a href="#" class="trip-link" id=' + trip.id + '>' + trip.name + '</a></td>');
+
+      row.append(name);
+      body.append(row);
+    });
+  };
+
+  var failCallback = function(xhr) {
+    console.log('failed - sorry!');
+  };
+
+  $('button').click(function() {
+    $.get(url, successCallback)
+      .fail(failCallback);
+  });
 
 });
