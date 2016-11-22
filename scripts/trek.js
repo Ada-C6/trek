85bd32e0a663119eaac815b1e716048d8943a6e7
Sarah Nahmias <nahmisa@gmail.com>
2016-11-22 14:55:17 -0800

    Adds ability to click a button and show all treks

--- scripts/trek.js
+++ scripts/trek.js
@@ -2,4 +2,35 @@ $(document).ready(function() {
   // Which URL do we want to 'get'?
   var url = 'https://trektravel.herokuapp.com/trips/';
 
+  var successCallback = function (response) {
+
+    var body = $('.travel-locations');
+    body.empty();
+
+    $.each(response, function(index, travel){
+      var listItem = $('<li></li>');
+      var trekList = $('<ul></ul>');
+      var travelName = $("<li><a href=# id=" + travel.id + ">" + travel.name + '</li>');
+      var continent = $('<li>' + travel.continent + '</li>');
+      var duration = $('<li>' + travel.weeks + ' weeks</li>');
+
+      trekList.append(travelName, continent, duration);
+      listItem.append(trekList);
+      body.append(listItem);
+    });
+
+    $('button').hide();
+  };
+
+  // FAILURE
+  var failCallback = function(xhr) {
+    console.log('failure');
+    console.log(xhr);
+  };
+
+  $('button').click(function() {
+    $.get(url, successCallback)
+      .fail(failCallback);
+  });
+
 });
