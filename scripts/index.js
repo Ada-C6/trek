96e99d2c1918192687f1dd7d5dc9461f98ce5ef0
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-11-22 15:12:38 -0800

    show fxs and renders working

--- scripts/index.js
+++ scripts/index.js
@@ -4,9 +4,6 @@ $(document).ready(function() {
 
   // Response
   var successCallback = function(response) {
-    console.log('sucess!');
-    console.log(response);
-
     var body = $('.travel-body');
     body.empty();
 
@@ -17,6 +14,8 @@ $(document).ready(function() {
       row.append(name);
       body.append(row);
     });
+
+    toggleTableView(true);
   };
 
   var failCallback = function(xhr) {
@@ -27,5 +26,43 @@ $(document).ready(function() {
     $.get(url, successCallback)
       .fail(failCallback);
   });
+
+  var showSuccess = function(trip) {
+    var section = $('.trip-details');
+    var id = $('<p><strong>ID: </strong>' + trip.id + '</p>');
+    var name = $('<p><strong>Name: </strong>' + trip.name + '</p>');
+    var destination = $('<p><strong>Destination: </strong>' + trip.destination + '</p>');
+    var continent = $('<p><strong>Continent: </strong>' + trip.continent + '</p>');
+    var about = $('<p><strong>About: </strong>' + trip.about + '</p>');
+    var category = $('<p><strong>Category: </strong>' + trip.category + '</p>');
+    var weeks = $('<p><strong>Weeks: </strong>' + trip.weeks + '</p>');
+    var cost = $('<p><strong>Cost: </strong>$' + trip.cost + '</p>');
+
+
+    section.empty();
+    section.append(id, name, destination, continent, about, category, weeks, cost);
+
+    toggleTableView(false);
+  };
+
+  var showFailure = function(xhr) {
+    var section = $('.trip-details');
+    section.html('Error has occurred. Please try another trip.');
+
+    toggleTableView(false);
+  };
+
+  $('tbody').on('click', '[href]', function() {
+    var id = $(this).attr('id');
+    var showUrl = url + '/' + id;
+    $.get(showUrl, showSuccess)
+      .fail(showFailure);
+  });
+
+  var toggleTableView = function(onIndicator) {
+    $('.trip-details').toggle(!onIndicator);
+    $('button').toggle(!onIndicator);
+    $('table').toggle(onIndicator);
+  };
 
 });
