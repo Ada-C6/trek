6b656e62270cc62cb9439bc79b9e440b9b2eb33e
Shari Meggs <smeggs1@gmail.com>
2016-11-22 15:53:37 -0800

    Can show a single trip

--- scripts/trek.js
+++ scripts/trek.js
@@ -1,22 +1,35 @@
 $(document).ready(function(){
   var url = 'https://trektravel.herokuapp.com/trips';
 
-  var successCall = function (response) {
+  var successCall = function(response) {
     var body = $('.places');
     $.each(response, function(index, trip ){
       var row = $('<tr> </tr>');
-      var name = $('<td>' + trip.name + '</td>');
+      var name = $('<td><a href="#" class="da" id=' + trip.id + '>' + trip.name + '</td>');
       var continent = $('<td>' + trip.continent + '</td>');
       var weeks = $('<td>' + trip.weeks + '</td>');
       row.append(name, continent, weeks);
       body.append(row);
     });
     var singleTrip = function(trips){
-
+      var section = $('.here');
+      var name = $('<strong>Trip: </strong><div>' + trips.name + '</div>');
+      var continent = $('<strong>Destination: </strong><div>' + trips.continent + '</div>');
+      var about = $('<strong>Description: </strong><div>' + trips.about + '</div>');
+      var category = $('<strong>Categotry: </strong><div>' + trips.category + '</div>');
+      var weeks = $('<strong>Weeks: </strong><div>' + trips.weeks + '</div>');
+      var cost = $('<strong>Cost: </strong><div>' + trips.cost + '</div>');
+      section.empty();
+      section.append(name, continent, about, category, weeks, cost);
+      };
+      $('tbody').on('click', 'a', function(e){
+        var id = $(this).attr('id');
+        var second = url + '/' + id;
+        $.get(second, singleTrip);
+      });
+      $('button').hide();
     };
-    $('button').hide();
-  };
-  $('button').on('click', function(){
-    $.get(url, successCall);
+    $('button').on('click', function(){
+      $.get(url, successCall);
+    });
   });
-});
