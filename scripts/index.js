483e29e5a134cef85ae1356aa0ddcf7ca4a13a39
Dianne Laguerta <diannelaguerta@gmail.com>
2016-11-22 15:12:24 -0800

    showing all trips works

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,43 @@ $(document).ready(function(){
 // console.log("script works!");
 
 
+var tripsurl = 'https://trektravel.herokuapp.com/trips';
 
 
+var successCallback = function (response) {
+    console.log('success!');
+
+    var body = $('.trips-body');
+    // body.empty();
+
+    $.each(response, function(index, trip){
+      console.log(trip);
+      var row = $('<tr></tr>');
+      var name = $('<td><h3><a href="#" class="name-link" id=' + trip.id + '>' + trip.name + '</h3></a></td>');
+
+      row.append(name);
+      body.append(row);
+    });
+
+    // toggleTableView(true);
+  };
+
+//if .get for receiving all trips fails
+  var failCallback = function(xhr) {
+    console.log('failure');
+    console.log(xhr);
+    var failmessage =  $('<h3> Sorry, something is wrong with our servers or this website in general.</h3>');
+    var body = $('.trips-body');
+    body.append(failmessage);
+  };
+
+
+
+// .get for loading all trips
+  $('button').click(function() {
+      $.get(tripsurl, successCallback)
+        .fail(failCallback);
+    });
 
 
 
