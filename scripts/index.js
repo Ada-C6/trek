7b2ff2db6dfd4d324030ac5f66cc5e3b458d97fc
guineveresaenger <guineveresaenger@yahoo.com>
2016-11-23 15:45:42 -0800

    Most of the functionality. Added minor button stylings. Reservation from toggles.

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
 
-  console.log("this is a thing");
+$(document).ready(function() {
   var url = "https://trektravel.herokuapp.com/trips";
+  $('#reservation-form').hide();
 
   var successCallback = function(response) {
     console.log("yay trips!");
@@ -12,29 +12,32 @@ $(document).ready(function() {
       var row = $('<tr></tr>');
       var name = $('<td><a href="#" class="name-link" id=' + trip.id + ">" + trip.name + '</a></td>');
       var continent = $('<td>' + trip.continent + '</td>');
-      var weeks = $('<td>' + trip.weeks + '</td>');
+      var weeks = $('<td>' + trip.weeks + ' week(s)</td>');
 
       row.append(name, continent, weeks);
       $('.trips-body').append(row);
 
     });
-    $('button').hide();
+    $('#trip-load-button').hide();
   };
 
   var tripCallback = function(trip) {
     console.log('this should be a single trip');
     console.log(trip);
-    $('body').empty();
+    $('.trip-display').empty();
+    $('#reservation-form').toggle();
     var name = $('<h2>' + trip.name + '</h2>');
     var duration = $('<div><span>Duration: </span>' + trip.weeks + ' week(s)</div>');
     var cost = $('<div><span>Cost: $</span>' + trip.cost + '</div>');
+    var id = $('<div><span>ID: </span><span class="trip-id">' + trip.id + "</span></div>");
     var continent = $('<div><span>Continent: </span>' + trip.continent + '</div>');
     var category = $('<div><span>Category: </span>' + trip.category + '</div>');
     var description = $('<div><span>Description: </span><br>' + trip.about + '</div>');
-    $('body').append(name, duration, cost, continent, category, description);
+    $('.trip-display').append(name, duration, cost, id, continent, category, description);
   };
 
   $('.trips-body').on('click', 'a', function(e){
+
     e.preventDefault();
     var id = $(this).attr('id');
     var tripUrl = url + "/" + id;
@@ -45,21 +48,43 @@ $(document).ready(function() {
   var failCallback = function(xhr) { //uses 'xhr' - 'XML header' - to denote all kinds of responses, including failures
     console.log("failure");
     console.log(xhr);
-    $('body').append("Sorry invalid URL but Heather is awesome<br>");
+    $('body').append("Sorry invalid URL!<br>");
 
   };
 
   var alwaysCallback = function() {
     console.log("this always happens");
-    $('body').append("This always happens but Heather is still awesome");
-
   };
 
-  $('button').click(function() {
+  $('#trip-load-button').click(function() {
       $.get(url, successCallback)
         .fail(failCallback)
         .always(alwaysCallback);
-    });
+  });
+
+
+  //POST
+
+  var postCallback = function(){
+    alert("you did it right! yay!");
+    console.log("did this work");
+  };
+
+  $('#reservation-form').submit(function(event){
+    var tripId =$(".trip-id").html();
+    var reservationUrl = url+ "/" + tripId + "/" + "reserve";
+    event.preventDefault();
+    var reservationData = $(this).serialize();
+    $.post(reservationUrl, reservationData, postCallback);
+
+  });
+
+  // var displayForm = function(){
+  //   var form = $('<form id="reservation-form" class="small-6 columns"><label for="name">Name</label><input type="text" name="name"></input><label for="email">Email</label><input type="text" name="email"></input><button type="submit" class="button">Reserve this trip!</button></form>');
+  //
+  // };
+
+
 
 
 });
