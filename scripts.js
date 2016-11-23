a452ec6f613aa8145ef7048f2dd3689fbe669e44
Alyssa Hursh <akhursh@gmail.com>
2016-11-23 13:23:00 -0800

    Pop-up window closes on second click

--- scripts.js
+++ scripts.js
@@ -8,22 +8,62 @@ $(document).ready(function(){
   var successCallback = function (response) {
     for (var i=0; i < response.length; i++ ){
       $('#vacations').append("<ul><li><a href=" + url + "/" + response[i].id + ">" + response[i].name + "</a></li><li> Continent:" + response[i].continent + "</li><li> Weeks:" + response[i].weeks + "</li></ul>");
-    }
-  };
+    } // end for loop
+  }; // end successCallbank function
 
   // Click to show the list, then hide the button;
   $('#show_vac').on('click', function(e) {
     $.get(url, successCallback);
     $("#show_vac").hide();
-  });
+  }); // end onclick function
 
+  // declare a variable (unassigned to start)
+  var openStatus;
+  console.log(openStatus);
 
+  // Start of vacations on link click function
   $('#vacations').on('click', 'a', function(e) {
+    // stop the click on a vacation link from going to url
     e.preventDefault();
 
+    // change display status div single-vac to block
     $('#single_vac').show();
 
-  });
+    // set local URL
+    var vacUrl = $(this).attr('href');
+
+    // make get request for all the details of a single vacation
+    $.get(vacUrl, function(vacation){
+      $('#name').text("Vacation Name: " + vacation.name);
+      $('#destination').text("Destination: " + vacation.destination);
+      $('#continent').text("Continent: " + vacation.continent);
+      $('#about').text("About: " + vacation.about);
+      $('#category').text("Category: " + vacation.category);
+      $('#weeks').text("Weeks: " + vacation.weeks);
+      $('#cost').text("Cost: $" + vacation.cost);
+      $('#id').text("Vacation ID:" + vacation.id);
+
+      // set the open status to true
+      openStatus = true;
+      console.log("inside the click function" + openStatus);
+
+    }); // end get function
+  }); // end of vacation on click function
+
+
+  // if the open status is true, any click should close the single_vac div.
+  $('body').on('click', function(){
+    if (openStatus === true) {
+      $("#single_vac").hide();
+      openStatus = false;
+    } // end of the if statement
+  }); // end of the body on click function
+
+}); // end of complete document.ready statement
+
+
+
+
 
 //click on a pet, show more information on that pet
 //click event, that will make an ajax get request for that specific pet, by ID
@@ -50,4 +90,4 @@ $(document).ready(function(){
 
 
 
-}); // ending $(document).ready
+// }); // ending $(document).ready
