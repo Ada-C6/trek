a8c3927e0d1e3bd7f2bfb338033cc76a29e8e263
Yeni <yenicapote08@gmail.com>
2016-11-23 15:00:27 -0800

    Added details on each trip feature. Included a scetion on the html file to contain the details.

--- trek.js
+++ trek.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
       row.append(name);
       body.append(row);
     });
-  
+    toggleTableView(true);
   };
 
   $('#load-trip-button').click(function() {
@@ -28,6 +28,40 @@ $(document).ready(function() {
       .fail(tripFailCallback);
   });
 
+  var toggleTableView = function(onIndicator) {
+    $('.trip-details').toggle(!onIndicator);
+    $('button').toggle(!onIndicator);
+    $('table').toggle(onIndicator);
+  };
 
+  var tripDetailSuccess = function(trip) {
+    var section = $('.trip-details');
+    var id = $('<strong>Id</strong><div>' + trip.id + '</div>')
+    var name = $('<strong>Name</strong><div>' + trip.name + '</div>');
+    var continent = $('<strong>Continent</strong><div>' + trip.continent + '</div>');
+    var about = $('<strong>About</strong><div>' + trip.about + '</div>');
+    var category = $('<strong>Category</strong><div>' + trip.category + '</div>');
+    var weeks = $('<strong>Weeks</strong><div>' + trip.weeks + '</div>');
+    var cost = $('<strong>Cost</strong><div>' + trip.cost + '</div>');
 
+    section.empty();
+    section.append(id, name, continent, about, category, weeks, cost);
+
+    toggleTableView(false);
+  };
+
+  var tripDetailFailure = function(fail) {
+    var section = $('.trip-details');
+    section.html('Error has occurred! Dun dun dun....');
+    toggleTableView(false);
+  }
+
+  $('tbody').on('click', 'a', function(event) {
+    event.preventDefault();
+
+    var id = $(this).attr('id');
+    var tripDetailUrl = url + '/' + id;
+    $.get(tripDetailUrl, tripDetailSuccess)
+      .fail(tripDetailFailure);
+  });
 });
