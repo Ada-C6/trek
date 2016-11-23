8cde0d3e938b6c83b7f8067a2910727b0c2e17be
Johna N. Morris <johna.n.morris@gmail.com>
2016-11-22 16:57:50 -0800

    wrote click actions for showing all trips and to show individual trips

---
+++ scripts/index.js
@@ -0,0 +1,87 @@
+// var toggleList = function(onIndicator) {
+//   $('.all-trips').toggle(!onIndicator);
+//   $('button').toggle(!onIndicator);
+//   $('ul').toggle(onIndicator);
+// };
+
+
+$(document).ready(function() {
+  var url = 'https://trektravel.herokuapp.com/trips';
+
+  var successCallback = function(response) {
+    console.log('success!');
+
+    var section = $('.all-trips');
+    section.empty();
+
+    $.each(response, function(index, trip) {
+      console.log(trip);
+      var listItem = $("<li></li>");
+      var innerList = $("<ul></ul>");
+      var name = $("<strong><a href='#' id=" + trip.id + ">" + trip.name + "</a></strong>");
+
+      var location = $("<li>Continent: " + trip.continent + "</li>");
+      var weeks = $("<li>Number of Weeks: " + trip.weeks + "</li>");
+
+      listItem.append(name, innerList);
+      innerList.append(location, weeks);
+      section.append(listItem);
+    });
+    $('button').hide();
+  };
+
+  var failCallback = function(xhr) {
+    console.log('failure');
+    console.log(xhr);
+    $('.all-trips').append("<h2>No information available</h2>");
+  };
+
+  var showSuccess = function(trip) {
+    var section = $(".show-trip");
+    section.empty();
+    var title = $("<h2></h2>");
+    var description = $("<div></div>");
+
+    var name = $("<span>" + trip.name + "</span>");
+    var continent = $("<small>" + trip.continent + "</small>");
+
+    var about = $("<p>" + trip.about + "</p>");
+    var category = $("<p><strong>Category: </strong>"+ trip.category + "</p>");
+    var weeks = $("<p><strong>Number of Weeks: </strong>" + trip.weeks + "</p>");
+    var cost = $("<p><strong>Price:</strong> $" + trip.cost + "</p>");
+
+    title.append(name, continent);
+    description.append(about, category, weeks, cost);
+
+    section.append(title, description);
+  };
+
+  $('button').click(function() {
+    $(".show-all").addClass("add-border");
+    $.get(url, successCallback)
+      .fail(failCallback);
+  });
+
+  $('.all-trips').on('click', 'a', function(event){
+    event.preventDefault();
+    var id = $(this).attr("id");
+    var showUrl = url + "/" + id;
+    $.get(showUrl, showSuccess);
+  });
+
+  // var showFailure = function(xhr) {
+  //   var section = $('.pet-details');
+  //   section.html('<strong>Error has occurred</strong>');
+  //
+  //   toggleTableView(false);
+  // };
+  //
+  // $('tbody').on('click', 'a', function(e) {
+  //   e.preventDefault();
+  //
+  //   var id = $(this).attr('id');
+  //   var showUrl = url + '/' + id;
+  //   $.get(showUrl, showSuccess)
+  //     .fail(showFailure);
+  // });
+});
