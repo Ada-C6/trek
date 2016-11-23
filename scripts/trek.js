ca63c4db983602b90769f8762ce4df051680d264
Kate Shaffer <kate.shaffer@gmail.com>
2016-11-23 15:30:25 -0800

    Sketched out addition of show/ID callback

--- scripts/trek.js
+++ scripts/trek.js
@@ -9,11 +9,26 @@ var tripListRow = function(trip) {
   return row;
 };
 
+var buildShowSection = function(trip) {
+  console.log("Ready to build a show section");
+  console.log("Trip: " + trip);
+  // var destination = trip.destination ;
+  var name = trip.name;
+  var id =  trip.id;
+  var continent = trip.continent;
+  var about = trip.about;
+  var category = trip.category;
+  var weeks = trip.week;
+  var cost = trip.cost;
+
+  $("#show-trip-section").append(/* @TODO - add elements here */);
+};
 
 $(document).ready(function() {
   var listUrl = "https://trektravel.herokuapp.com/trips";
 
-  var listSuccess = function(response) {
+  // List Callbacks
+  var listSuccessCallback = function(response) {
     console.log("I'm the success callback");
     console.log("Response: " + response);
 
@@ -24,8 +39,36 @@ $(document).ready(function() {
     });
   };
 
-  $("#show-button").on('click', function() {
-    console.log("I will do the thing!");
-    $.get(listUrl, listSuccess);
+  var listFailCallback = function(xhr) {
+    console.log('failure');
+    console.log(xhr);
+  };
+
+  // ID Callbacks
+  var idSuccessCallback = function(response) {
+    console.log("ID response: " + response);
+    $('#show-trip-section').empty();
+    $('#show-trip-section').append(buildShowSection(response));
+  };
+
+
+  // Events
+  $("#show-list-button").on('click', function(event) {
+    console.log("I will do the list thing!");
+    $.get(listUrl, listSuccessCallback)
+      .fail(listFailCallback);
+  });
+
+  $('#trip-list-table').on('click', 'a', function() {
+    console.log(this);
+    console.log('you clicked a trip');
+    var idForUrl = $(this).attr('id');
+    console.log("ID: " + idForUrl);
+
+    var idUrl = listUrl + "/" + idForUrl;
+    console.log(idUrl);
+    $.get(idUrl, idSuccessCallback);
+      // .fail(id_failCallback)
+      // .always(id_alwaysCallback);
   });
 });
