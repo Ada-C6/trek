c0ad1598534afafe07357a14852410138dc70f4b
Karin Kubischta <karin.kubischta@gmail.com>
2016-11-22 20:54:01 -0800

    wave 2, with money formatting

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ $(document).ready(function(){
   // what do we want to happen when we get a response?
   var successCallback = function(response) {
     console.log('success!');
-    // console.log(response);
+
+    var tableHeaders = Object.keys(response[0]);
+    // start at 1, because the first key is the 'id', which I don't want to display.
+    for (var j = 1; j<tableHeaders.length; j++){
+      var headings = $('<th>'+ tableHeaders[j] +'</th>');
+      $('.trips-header').append(headings);
+    }
+
     var body = $('.trips-body');
     body.empty(); // clearing the body out to ensure populating fresh.
 
     $.each(response, function(index, trip){
       // console.log(pet);
-      var row = $('<tr></tr>');
-      var name = $('<td>' + trip.name + '</td>');
+      var row = $('<tr id='+ trip.id + '></tr>');
+      var name = $('<td>'+ trip.name + '</td>');
       var continent = $('<td>' + trip.continent + '</td>');
       var duration = $('<td>' + trip.weeks + '</td>');
 
@@ -40,4 +47,31 @@ $(document).ready(function(){
       .always(alwaysCallback);
   });
 
+  var showSuccess = function(trip) {
+    var section = $('.trip-details');
+    var name = $('<strong>Name</strong><div>' + trip.name + '</div>');
+    var continent = $('<strong>Continent</strong><div>' + trip.continent + '<div>');
+    var description = $('<strong>Description</strong><div>' + trip.about + '</div>');
+    var category = $('<strong>Category</strong><div>' + trip.category + '</div>');
+    var cost = $('<strong>Cost</strong><div>' + trip.cost.toLocaleString('us-US', { style: 'currency', currency: 'USD' }) + '</div>');
+    var duration = $('<strong>Duration</strong><div>' + trip.weeks + ' week(s)</div>');
+
+    section.empty(); //similar to the body, want to make sure we don't have any data from before
+    section.append(name, continent, description, category, cost, duration);
+  };
+
+  var showFailure = function(xhr) {
+    var section = $('.trip-details');
+    section.html('<strong>Error has occurred</strong>');
+  };
+
+  $('tbody').on('click','tr', function(e){
+    e.preventDefault(); // this captures the event of a click on a link and prevents the default action from happening so that we can use ajax instead of an href?
+    console.log($(this));
+
+    var id = $(this).attr('id');
+    var showUrl = url + '/' + id;
+    $.get(showUrl, showSuccess)
+      .fail(showFailure);
+    });
 });
