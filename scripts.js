26d9a2d30de9d28c6fd229dc07c367280be5a841
“BJHunnicutt” <jeannie.hunnicutt@gmail.com>
2016-11-22 19:51:13 -0800

    single trip popup and some formatting

--- scripts.js
+++ scripts.js
@@ -9,8 +9,10 @@ var url = 'https://trektravel.herokuapp.com/trips';
 // Print out a list of response links
 var successCallback = function (response) {
   for (var i = 0; i < response.length; i++ ) {
-  $('#trips').append(" <h3> <a href=" + url + "/" + response[i].id + ">" + response[i].name + "</a></h3>");
+  $('#trips').append(" <div class='trip-list'> <h3> <a href=" + url + "/" + response[i].id + ">" + response[i].name + "</a></h3></div> ");
+
   // console.log('success!');
+  $('#load').hide(); // So you can't keep apending.
   }
 };
 
@@ -21,66 +23,60 @@ $("#load").click(function() {
 });
 
 
-//
-// // When I click on a pet, show more info on that trip in a popup...
-// $('#pets').on('click', 'a', function(e) { // pass in e (shorthand for event)
-//   e.preventDefault();
-//   $("#profile").show(); //Switches display: none;  ** To go back:  $("#profile").hide();
-//
-//   var petUrl = $(this).attr('href');
-//
-//   // the second value in the get request is the response passed in if the request was successful **the function(pet) here** will run
-//   // If there is an unsuccessful request, a 404 will be returned and the function won't run
-//   var pet = $.get("petUrl", function(pet) { //changed to function(response) from successCallback, which had given us an object with a ton of data
-//     // console.log(response);
-//     $('#name').text(pet.name);
-//     $('#age').text(pet.age);
-//     $('#breed').text(pet.breed);
-//   }).fail(function() {
-//     console.log('Failure.');
-//   }).always(function() {
-//     $('#message').text('Something Happened!');
-//   });
-//
-// });
 
+// ------------- Display trip pop-up -------------- // Parts adapted from from: http://stackoverflow.com/questions/1328723/how-to-generate-a-simple-popup-using-jquery   &   http://stackoverflow.com/questions/28558865/popup-window-close-on-click-outside-not-inside-jquery
+$('#trips').on('click', 'a', function(event) {
+  window.scrollTo(0,0);
+  
+  if($(this).hasClass('selected')) {
+    deselect($(this));
+  } else {
+    $(this).addClass('selected');
+    $('.pop').fadeToggle("fast");
+  }
 
-// Display trip pop-up //
-function deselect(e) {
-  $('.pop').slideFadeToggle(function() {
-    e.removeClass('selected');
+  event.preventDefault();
+  var tripUrl = $(this).attr('href');
+
+  // Should be able to see id, name, destination, continent, about, category, weeks and cost
+  var tripResponse = $.get(tripUrl, function(trip) {
+    $('#trip-id').text("Trip ID: " + trip.id);
+    $('#trip-name').text(trip.name);
+    $('#trip-continent').text(trip.continent);
+    $('#trip-category').text(trip.category);
+    $('#trip-weeks').text(trip.weeks);
+    $('#trip-cost').text(trip.cost);
+    $('#trip-description').text(trip.about);
+  }).fail(function() {
+    console.log('Failure.');
+  }).always(function() {
+    $('#test-message').text('Something Happened!');
   });
-}
-
-$(function() {
-  $('#contact').on('click', function() {
-    if($(this).hasClass('selected')) {
-      deselect($(this));
-    } else {
-      $(this).addClass('selected');
-      $('.pop').slideFadeToggle();
-    }
-    return false;
-  });
-
-  $('.close').on('click', function() {
-    deselect($('#contact'));
-    return false;
-  });
+  // return false;
 });
 
-$.fn.slideFadeToggle = function(easing, callback) {
-  return this.animate({ opacity: 'toggle', height: 'toggle' }, 'fast', easing, callback);
+// Deselect a pop-up
+var deselect = function(e) {
+  $('.pop').fadeToggle("fast", function() {
+    e.removeClass('selected');
+  });
 };
 
+// Deselect a pop-up if you hit close
+$('.close').on('click', function() {
+  deselect($('#trips'));
+  return false;
+});
 
+// Close pop-up if you click anywhere besides the pop-up
+$(document).mouseup(function (e) {
+     var popup = $(".pop");
+     if (!$('#trips').is(e.target) && !popup.is(e.target) && popup.has(e.target).length === 0) {
+         popup.hide(200);
+     }
+ });
 
-
-
-
-
-
-
+// ------------- Display trip pop-up -------------- //
 
 
 
