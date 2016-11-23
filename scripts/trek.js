597453c4e8f5a99a51c5bbacec85f47fca4a18c3
Kate Shaffer <kate.shaffer@gmail.com>
2016-11-23 12:26:54 -0800

    Built trips list callback with basic table display. Added foundation CDN.

--- scripts/trek.js
+++ scripts/trek.js
@@ -1,3 +1,31 @@
+var tripListRow = function(trip) {
+  var row = $('<tr></tr>');
+  var id = trip.id;
+  var name = $('<td class="details"><a href="#" id=' + trip.id + '>' + trip.name + '</a></td>');
+  var continent = $('<td>' + trip.continent + '</td>');
+  var weeks = $('<td>' + trip.weeks + '</td>');
+
+  row.append(name, continent, weeks);
+  return row;
+};
+
+
 $(document).ready(function() {
-  $('body').append("<h1>Hello</h1>");
+  var listUrl = "https://trektravel.herokuapp.com/trips";
+
+  var listSuccess = function(response) {
+    console.log("I'm the success callback");
+    console.log("Response: " + response);
+
+    $.each(response, function(index, trip) {
+      console.log(trip);
+      var addRow = tripListRow(trip);
+      $('#trip-list-table > tbody').append(addRow);
+    });
+  };
+
+  $("#show-button").on('click', function() {
+    console.log("I will do the thing!");
+    $.get(listUrl, listSuccess);
+  });
 });
