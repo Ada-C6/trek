432fdc8f53ed3e4b1e6b9e2b4965713145fb9204
Melissa Rodriguez <olenellina@gmail.com>
2016-11-22 21:05:41 -0800

    improvements to scrollbar/trip section

--- trek.js
+++ trek.js
@@ -1,13 +1,22 @@
+// AJAX Notes:
+// AJAX is a design pattern that you can implement in jQuery (but also plain JS)
+// Requires server communication - async communication with server using JS.
+// Entails composing request objects, sending to server and async waiting for the response.
+// Previously, when you made a JS request to server it was a blocking request,
+// nothing else could happen until that response came through.
+// Nothing below is AJAX code per se, but rather how I'm using jQuery.
+
 $(document).ready(function(){
 
   $( "#load" ).click(function() {
       var url = "https://trektravel.herokuapp.com/trips"
       $.get(url,
         function(response){
-                for (var i = 0; i < response.length; i++){
-                  console.log(response[i]);
-                  $('#trips').append("<div class='record'><a href=" + url + "/" + response[i].id + ">" + response[i].name + "</a></div>");
-                }
+          $('#trips').append("<h3> Select a trip for more information: </h3>")
+          for (var i = 0; i < response.length; i++){
+            console.log(response[i]);
+            $('#trips').append("<div class='record'><a href=" + url + "/" + response[i].id + ">" + response[i].name + "</a></div>");
+          }
         });
     });
 
