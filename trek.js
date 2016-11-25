0e635a44234d7a1dee3cdb914167637942101866
Briana Eng <brianaeng@gmail.com>
2016-11-25 00:05:18 -0800

    Added comments

--- trek.js
+++ trek.js
@@ -1,28 +1,32 @@
 $(document).ready(function() {
+  //Base url for get & post requests
   var baseUrl = "https://trektravel.herokuapp.com/trips";
 
+  //Function to iterate through trips and create a button for each
   var getTrips = function (trips){
     for (var i = 0; i < trips.length; i++) {
       $(".list-trips").append("<div><button class='trip'>" + trips[i].id + "</button></div>");
     }
   };
 
+  //Function to display the confirmation section
   var confirmationAlert = function(){
     $(".confirmation").show();
   };
 
+  //Function to show the new trip form and hide the welcome image & confirmation section
   $(".add-trip").on('click', function(){
     $(".new-trip").show();
     $(".welcome-image, .confirmation").hide();
   });
 
+  //Function to submit the form for a new trip as a post request.
   $("form[id=create-trip]").submit(function(e) {
     e.preventDefault();
 
     var tripDetails = $(this).serialize();
     var newTripUrl = baseUrl;
     $(".confirmation").text("You successfully created a new trip!");
-    console.log("created trip");
     $.post(newTripUrl, tripDetails, confirmationAlert);
 
     $(".new-trip").hide();
@@ -30,15 +34,18 @@ $(document).ready(function() {
     $('input').val('');
   });
 
+  //Function to show all of the trips through a get request and hide unnecessary elements
   $(".show-trips").on('click', function() {
     $.get(baseUrl, getTrips);
     $(".show-trips, .welcome-image, .new-trip, .add-trip, .confirmation").hide();
   });
 
+  //Function to close the single trip card
   $("#close-trip").on('click', function() {
     $(".standout-trip").hide();
   });
 
+  //Function to show a single trip through a get request. Function to handle reservations through a post request is also nested within.
   $(".list-trips").on("click", ".trip", function(trip) {
     $(".confirmation").hide();
     var tripId = $(this).html();
@@ -51,7 +58,6 @@ $(document).ready(function() {
       $("#category").text("CATEGORY: " + trip.category);
       $("#weeks").text("WEEKS: " + trip.weeks);
       $("#about").text("ABOUT: " + trip.about);
-      console.log(trip);
       $(".standout-trip").show();
     };
 
