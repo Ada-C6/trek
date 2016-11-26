fe02011211650f8ff35e45dd67a205d22ff42236
Nicole Url <nicole.url.4@gmail.com>
2016-11-26 11:14:32 -0800

    Reservation post request works

--- scripts/trek.js
+++ scripts/trek.js
@@ -1,82 +1,97 @@
 $(document).ready(function() {
-  ////////// API URL
-    var url = 'https://trektravel.herokuapp.com/trips';
+  showLandingView();
 
-  ////////// GET REQUEST
-    $('button').on('click', function(){
-      $.get(url, successCallback)
-        .fail(failCallback);
-      toggleIndexView(true);
+////////// API URL
+  var url = 'https://trektravel.herokuapp.com/trips';
+
+////////// GET REQUEST
+  $('.index-button').on('click', function() {
+    showIndexView(successCallback, failCallback)
+  })
+
+////////// GET CALLBACKS
+  var successCallback = function (response) {
+    $.each(response, function(index, trip) {
+      var row = $("<tr></tr>"),
+          name = $('<td><a href="#" id=' + trip.id + ">" + trip.name + "</a></td>"),
+          continent = $("<td>" + trip.continent + "</td>");
+
+      row.append(name, continent);
+      $(".trips-tbody").append(row);
     })
+  };
 
-  ////////// GET CALLBACKS
-    var successCallback = function (response) {
-      $.each(response, function(index, trip) {
-        var row = $("<tr></tr>"),
-            name = $('<td><a href="#" class="name-link" id=' + trip.id + ">" + trip.name + "</a></td>"),
-            continent = $("<td>" + trip.continent + "</td>");
+  var failCallback = function(xhr) {
+    alert("There has been a problem, please refresh.");
+  };
 
-        row.append(name, continent);
-        $(".trips-tbody").append(row);
-      })
-    };
+///////// SHOW
+  $(".trips-tbody").on("click", "a", function() {
+    var tripId = $(this).attr('id'),
+        showUrl = url + '/' + tripId;
 
-    var failCallback = function(xhr) {
-      alert("There has been a problem, please refresh.");
-    };
+    $.get(showUrl, showCallback);
 
-  ///////// SHOW
-    $(".trips-tbody").on("click", "a", function() {
-      var tripId = $(this).attr('id');
-      // console.log(pets);
-
-      var showUrl = url + '/' + tripId;
-      $.get(showUrl, showCallback);
-    });
+    showTripView();
+  });
 
 
-    var showCallback = function(trip) {
-      var name = $("<h4>Name</h4><p>" + trip.name + "</p>"),
-          continent = $("<h4>Continent</h4><p>" + trip.continent + "</p>"),
-          category = $("<h4>Category</h4><p>" + trip.category + "</p>"),
-          weeks = $("<h4>Amount of Weeks</h4><p>" + trip.weeks + "</p>"),
-          cost = $("<h4>Cost</h4><p>$" + trip.cost + "</p>"),
-          about = $("<h4>About</h4><p>" + trip.about + "</p>")
+  var showCallback = function(trip) {
+    var tripId = $("<h6>Trip Id</h6><p>" + trip.id + "</p>"),
+        name = $("<h4>Name</h4><p>" + trip.name + "</p>"),
+        continent = $("<h4>Continent</h4><p>" + trip.continent + "</p>"),
+        category = $("<h4>Category</h4><p>" + trip.category + "</p>"),
+        weeks = $("<h4>Amount of Weeks</h4><p>" + trip.weeks + "</p>"),
+        cost = $("<h4>Cost</h4><p>$" + trip.cost + "</p>"),
+        about = $("<h4>About</h4><p>" + trip.about + "</p>");
 
-      $("#show-trip").empty();
-      $("#show-trip").append(name, continent, category, weeks, cost, about);
+    $("#show-trip").empty();
+    // $("#booking-form").empty();
+    $("#show-trip").append(tripId, name, continent, category, weeks, cost, about);
+    $("#hidden-id-field").val(trip.id)
 
-      toggleIndexView(false);
-      // toggleBookingView(false);
-    };
+    showTripView();
+  };
 
-  ////////// VIEW
-    var toggleIndexView = function(isOn) {
-      $("#show").toggle(!isOn);
-      $("#booking-form").toggle(!isOn);
-      $("#index").toggle(isOn);
-    }
+////////// VIEWS
+  var speed = 1000; // thanks, kai
 
-    // var toggleBookingView = function(isOn) {
-    //   $("#show").toggle(!isOn);
-    //   $("#index").toggle(!isOn);
-    //   $("#book").toggle(isOn);
-    // }
+  function showLandingView() {
+    $("#show").hide(speed);
+    $("#index").hide(speed);
+  }
 
-  ////////// POST
-    var addTripCallback = function(event) {
-      event.preventDefault();
+  function showIndexView(successCallback, failCallback) {
+    $("#show").hide(speed);
+    $("#index").show(speed);
 
-      var tripData = $(this).serialize();
-      console.log("trip data = " + tripData)
-      $.post(url, tripData, postCallback);
+    $.get(url, successCallback)
+      .fail(failCallback);
+  }
 
-      toggleIndexView(true);
-    };
+  function showTripView() {
+    $("#show").show(speed);
+    $("#index").hide(speed);
+  }
 
-    $("#add-trip-form").submit(addTripCallback);
+////////// POST
+  var addTripCallback = function(event) {
+    event.preventDefault();
 
-    var postCallback = function() {
-      alert("Post success!");
-    }
+    var tripData = $(this).serialize(),
+        tripId = $("#hidden-id-field").val();
+
+    console.log("trip data = " + tripData)
+
+    var reservationUrl = url + "/" + tripId + "/reserve";
+    $.post(reservationUrl, tripData, postCallback);
+
+    showIndexView();
+  };
+
+  $("#add-reservation").submit(addTripCallback);
+
+  var postCallback = function() {
+    alert("Reservation made!");
+  }
 });
