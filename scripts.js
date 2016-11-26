717401196edbdeff4e6c1b959e1602994fc91fc4
“BJHunnicutt” <jeannie.hunnicutt@gmail.com>
2016-11-26 14:29:37 -0800

    finished wave 3

--- scripts.js
+++ scripts.js
@@ -5,11 +5,14 @@ $(document).ready( function() {
 
 // Trek API url to 'get'?
 var url = 'https://trektravel.herokuapp.com/trips';
+$('#trips').hide();
 
 // Print out a list of response links
 var successCallback = function (response) {
+  $('#trips').show();
+
   for (var i = 0; i < response.length; i++ ) {
-  $('#trips').append(" <div class='trip-list'> <h3> <a href=" + url + "/" + response[i].id + ">" + response[i].name + "</a></h3></div> ");
+  $('#trips').append("<h3 class='trip-list'> <a href=" + url + "/" + response[i].id + ">" + response[i].name + "</a></h3>");
 
   // console.log('success!');
   $('#load').hide(); // So you can't keep apending.
@@ -26,8 +29,14 @@ $("#load").click(function() {
 
 // ------------- Display trip pop-up -------------- // Parts adapted from from: http://stackoverflow.com/questions/1328723/how-to-generate-a-simple-popup-using-jquery   &   http://stackoverflow.com/questions/28558865/popup-window-close-on-click-outside-not-inside-jquery
 $('#trips').on('click', 'a', function(event) {
-  window.scrollTo(0,0);
-  
+  // // Get the position of the clicked element
+  // var posX = $(this).offset().left;
+  // var posY = $(this).offset().top;
+  // // make sure the fiew goes where the popup is
+  // // window.scrollTo(posX, posY);
+  // // Make the pop up happen at the location of the click
+  // $(".trippop").css({top: posY,});
+
   if($(this).hasClass('selected')) {
     deselect($(this));
   } else {
@@ -47,6 +56,8 @@ $('#trips').on('click', 'a', function(event) {
     $('#trip-weeks').text(trip.weeks);
     $('#trip-cost').text(trip.cost);
     $('#trip-description').text(trip.about);
+    // Set form submission url
+    $('#new_trip').attr('action', tripUrl + '/reserve');
   }).fail(function() {
     console.log('Failure.');
   }).always(function() {
@@ -79,9 +90,20 @@ $(document).mouseup(function (e) {
 // ------------- Display trip pop-up -------------- //
 
 
+var callback = function() {
+  console.log("success!");
+};
 
+$('form').submit(function(e){ // THe $(this) referred to the button above - now it refers to the submitted data
+  e.preventDefault(); // You have to prevent the default, because it's trying to make a new pet with no data automatically, but we want to control it with jQuery
+  var formData = $(this).serialize();
+  var postUrl = $(this).attr('action');
+  console.log(postUrl);
 
-
+  alert('Thank you! Your reservation has been submitted!');
+  $.post(postUrl, formData, callback);  //With this version you can get rid of the ids on the form
+  $( ".close" ).trigger("click");
+});
 
 
 }); // ending $(document).ready
