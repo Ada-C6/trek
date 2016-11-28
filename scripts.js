90497d91083eae987fdeb4f88fce09f8c8cdbf04
Leah Bueing <lbueing@gmail.com>
2016-11-27 22:09:58 -0800

    pop up successfully showing up

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 
 // Which URL do we want to 'get'?
@@ -6,7 +7,7 @@ var url = 'https://trektravel.herokuapp.com/trips';
 // What do we want to happen when we get our response?
 var successCallback = function (response) {
   for (var i = 0; i < response.length; i++ ) {
-    $('#all_trips').append("<h3><a href=" + url + "/" + response[i].id + ">" + response[i].name + "</a></h3>");
+    $('#all_trips').append("<h3><a data-open='trip_info' href=" + url + "/" + response[i].id + ">" + response[i].name + "</a></h3>");
   }
 };
 
@@ -16,4 +17,19 @@ $('body').on('click', 'button', function() {
   $.get(url, successCallback);
 });
 
+$('#all_trips').on('click', 'a', function(event) {
+  event.preventDefault();
+  $('#trip_info').show();
+  var tripUrl = $(this).attr('href');
+  $.get(tripUrl, function(trip) {
+    $('#name').text(trip.name);
+    $('#id').text(trip.id);
+    $('#continent').text(trip.continent);
+    $('#about').text(trip.about);
+    $('#category').text(trip.category);
+    $('#weeks').text(trip.weeks);
+    $('#cost').text('$' + trip.cost);
+  });
 });
+
+}); //end
