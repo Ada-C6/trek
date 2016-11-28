60fb80d53ae1725200b205a739ef09966ac809db
Carmen Chan <carmenkellychan@gmail.com>
2016-11-27 22:20:27 -0800

    wave3 in progress

--- trek.js
+++ trek.js
@@ -50,3 +50,55 @@ $('#trips').on('click', "a", function(e){
 // $('#show-details').click(function(event){
 //     event.stopPropagation();
 // });
+
+$("#reserve").on('click', function(){
+
+
+ $('form').submit(function(e) {
+  // By default, the form will attempt to do it's own local POST so we want to prevent that default behavior
+  e.preventDefault();
+
+  var url = $(this).attr("action"); // Retrieve the action from the form
+  var formData = $(this).serialize();
+
+  $.post(url, formData, function(response){
+  $('#message').html('<p> Trip added! </p>');
+
+  // What do we get in the response?
+  console.log(response);
+})
+  .fail(function(){
+    $('#message').html('<p>Adding Trip Failed</p>');
+  });
+});
+});
+
+
+// ########## script for "back to top" button ###############
+
+$("#back-top").hide();
+
+// fade in #back-top
+$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#back-top').fadeIn();
+    } else {
+      $('#back-top').fadeOut();
+    }
+  });
+
+  $("#back-top").click(function (){
+      $('html, body').animate({
+          scrollTop: $("body").offset().top
+      }, 500);
+  });
+
+  // scroll body to 0px on click
+  $('#back-top .fi-arrow-up').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
+});
