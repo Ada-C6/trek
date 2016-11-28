01927195b8dfb1bc8ad3b6b0cf747772e087180a
Kate Shaffer <kate.shaffer@gmail.com>
2016-11-27 17:44:42 -0800

    Built add reservation functionality. Need to fix a bug with clearing trip form info after submission.

--- scripts/trek.js
+++ scripts/trek.js
@@ -3,7 +3,11 @@
 // @TODO: properly handle failures?
 // @TODO: remove console.logs!
 
+// Build HTML section functions
 var tripListRow = function(trip) {
+  $('#show-trip-section').empty();
+  $('#reserve-trip-section').hide();
+
   var row = $('<tr></tr>');
   var id = trip.id;
   var name = $('<td class="details"><a href="#" id=' + trip.id + '>' + trip.name + '</a></td>');
@@ -26,15 +30,24 @@ var buildShowSection = function(trip) {
   var id = $('<h5> Trip Number: ' + trip.id + '</h5>');
   var cost = $('<h5> Cost: $' + trip.cost + '</h5>');
 
+  var formIdField = $('<input type="hidden" name="id" value="' + trip.id + '"/>');
+
   $('#trip-list-section').hide();
-  $('#show-trip-section').empty();
-  $('#show-trip-section').append(name, continent, weeks, category, about, cost, id);
+  $('#show-trip-section').prepend(name, continent, weeks, category, about, cost, id);
+  $('#reserve-trip-section').show();
+  $('#reserve-trip-form').prepend(formIdField);
 };
+
+// @TODO - remove if unused
+// var buildReservationForm = function(trip) {
+// };
+
+
 
 $(document).ready(function() {
   var listUrl = "https://trektravel.herokuapp.com/trips";
 
-  // List Callback
+  // List Callback function
   var listSuccessCallback = function(response) {
     console.log("I'm the success callback");
     console.log("Response: " + response);
@@ -54,13 +67,7 @@ $(document).ready(function() {
     });
   };
 
-  // FailCallback (used for all failures)
-  var FailCallback = function(xhr) {
-    console.log('failure');
-    console.log(xhr);
-  };
-
-  // ID Callback
+  // ID Callback function
   var idSuccessCallback = function(response) {
     console.log("ID response: " + response);
     $('#show-list-button').show();
@@ -68,6 +75,22 @@ $(document).ready(function() {
     $('#show-trip-section').append(buildShowSection(response));
   };
 
+  // Post Callback function (Reservation)
+  var postSuccessCallback = function(response) {
+    console.log("post response: " + response);
+    // $('#show-list-button').show();
+    $('#show-trip-section').prepend('<p>Reservation added! </p>');
+  };
+
+  // FailCallback (used for all failures)
+  var FailCallback = function(xhr) {
+    console.log('failure');
+    console.log(xhr);
+  };
+
+
+  // Initial page load should have #reserve-trip-section hidden
+  $('#reserve-trip-section').hide();
 
   // Events
   $("#show-list-button").on('click', function(event) {
@@ -76,7 +99,7 @@ $(document).ready(function() {
       .fail(FailCallback);
   });
 
-  $('#trip-list-table').on('click', 'a', function() {
+  $('#trip-list-table').on('click', 'a', function(event) {
     console.log(this);
     console.log('you clicked a trip');
     var idForUrl = $(this).attr('id');
@@ -86,5 +109,18 @@ $(document).ready(function() {
     console.log(idUrl);
     $.get(idUrl, idSuccessCallback)
       .fail(FailCallback);
+  });
+
+  $('#reserve-trip-form').submit(function(event) {
+    // @TODO - successfully pull id for URL
+    console.log("in Trip Reservation event");
+    event.preventDefault();
+    console.log("this: " + $(this));
+    var reservationData = $(this).serialize();
+    var tripId = this.id.value;
+    console.log("tripId: " + tripId);
+    var reserveUrl = listUrl + "/" + tripId + "/reserve";
+    console.log("reservationData is: " + reservationData);
+    $.post(reserveUrl, reservationData, postSuccessCallback);
   });
 });
