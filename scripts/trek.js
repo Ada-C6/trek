fddff92eb2e97303ce7c7aafe089613103a6f1b5
Shari Meggs <smeggs1@gmail.com>
2016-11-29 20:51:22 -0800

    sending over the id works

--- scripts/trek.js
+++ scripts/trek.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   var url = 'https://trektravel.herokuapp.com/trips';
-
   var successCall = function(response) {
+
     var body = $('.places');
     $.each(response, function(index, trip ){
       var row = $('<tr> </tr>');
@@ -13,6 +13,7 @@ $(document).ready(function(){
     });
     var singleTrip = function(trips){
       var section = $('.here');
+
       var name = $('<strong>Trip: </strong><div>' + trips.name + '</div>');
       var continent = $('<strong>Destination: </strong><div>' + trips.continent + '</div>');
       var about = $('<strong>Description: </strong><div>' + trips.about + '</div>');
@@ -20,16 +21,39 @@ $(document).ready(function(){
       var weeks = $('<strong>Weeks: </strong><div>' + trips.weeks + '</div>');
       var cost = $('<strong>Cost: </strong><div>' + trips.cost + '</div>');
       section.empty();
-      section.append(name,  about, continent, category, weeks, cost);
-      };
-      $('tbody').on('click', 'a', function(e){
-        var id = $(this).attr('id');
-        var second = url + '/' + id;
-        $.get(second, singleTrip);
-      });
-      $('button').hide();
+      section.append( name,  about, continent, category, weeks, cost);
+
+      var form = $('.new-res');
+      var id = $('<input type="hidden" value='+ trips.id + ' </input>');
+      form.append(id);
+
+
+
     };
-    $('button').on('click', function(){
-      $.get(url, successCall);
+
+    $('tbody').on('click', 'a', function(e){
+      var id = $(this).attr('id');
+      var second = url + '/' + id;
+      $.get(second, singleTrip);
     });
+
+    $('#see-trip').hide();
+  };
+  $('button').on('click', function(){
+    $.get(url, successCall);
   });
+
+  var postCallback = function(){
+    alert("Reservation Locked");
+  };
+  var addReservationCallback = function(event){
+    event.preventDefault(); //Stops it in it's tracks mwhaha
+    var new_url = url + '/' + $('input[type=hidden]').val() + '/reserve';
+    console.log("reservation confirmed!");
+
+    var  reservationInfo = $(this).serialize();
+    $.post(new_url, reservationInfo, postCallback);
+  };
+
+  $('#add-res').submit(addReservationCallback);
+});
