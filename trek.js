ae269abd63539b213da24e9ad565aaa85674cc95
Briana Eng <brianaeng@gmail.com>
2016-12-04 20:11:04 -0800

    Since this is already turned in and graded, added some fun so that you can only open and see a trip if the day has passed or is current day (like a real advent calendar)

--- trek.js
+++ trek.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   //Base url for get & post requests
   var baseUrl = "https://trektravel.herokuapp.com/trips";
+  var targetDate = new Date(2016, 11, 1);
 
   //Function to iterate through trips and create a button for each
   var getTrips = function (trips){
@@ -45,36 +46,60 @@ $(document).ready(function() {
     $(".standout-trip").hide();
   });
 
+  //Function to clear single trip/warning section
+  var clearTrip = function(){
+    $("#warning").text("");
+    $("#number").text("");
+    $("#cost").text("");
+    $("#continent").text("");
+    $("#category").text("");
+    $("#weeks").text("");
+    $("#about").text("");
+  };
+
   //Function to show a single trip through a get request. Function to handle reservations through a post request is also nested within.
   $(".list-trips").on("click", ".trip", function(trip) {
     $(".confirmation").hide();
+    clearTrip();
+
+    var currentDate = Date.now();
+    var daysDiff = (currentDate - targetDate)/(1000*60*60*24);
     var tripId = $(this).html();
-    var tripUrl = baseUrl + "/" + tripId;
 
-    var getOneTrip = function(trip) {
-      $("#number").text("# " + trip.id + " | " + trip.name.toUpperCase());
-      $("#cost").text("COST: $" + Math.trunc(trip.cost) + " (+ fees)");
-      $("#continent").text("CONTINENT: " + trip.continent);
-      $("#category").text("CATEGORY: " + trip.category);
-      $("#weeks").text("WEEKS: " + trip.weeks);
-      $("#about").text("ABOUT: " + trip.about);
+    if (Math.ceil(daysDiff) >= tripId) {
+      var tripUrl = baseUrl + "/" + tripId;
+
+      var getOneTrip = function(trip) {
+        $("#number").text("# " + trip.id + " | " + trip.name.toUpperCase());
+        $("#cost").text("COST: $" + Math.trunc(trip.cost) + " (+ fees)");
+        $("#continent").text("CONTINENT: " + trip.continent);
+        $("#category").text("CATEGORY: " + trip.category);
+        $("#weeks").text("WEEKS: " + trip.weeks);
+        $("#about").text("ABOUT: " + trip.about);
+        $(".standout-trip").show();
+      };
+
+      $.get(tripUrl, getOneTrip);
+
+      $("form[id=join-trip]").submit(function(e){
+        e.preventDefault();
+
+        var userDetails = $(this).serialize();
+        var userUrl = baseUrl + "/" + tripId + "/reserve";
+
+        $(".confirmation").text("You successfully reserved a spot on trip " + tripId + "!");
+
+        var response = $.post(userUrl, userDetails, confirmationAlert);
+        console.log(response);
+        $(".standout-trip").hide();
+        $('input[type="text"]').val('');
+      });
+    }
+    else {
+      $("#join-trip").hide();
+      $("#warning").text("Ho ho ho, you can't see this yet!");
       $(".standout-trip").show();
-    };
-
-    $.get(tripUrl, getOneTrip);
-
-    $("form[id=join-trip]").submit(function(e){
-      e.preventDefault();
-
-      var userDetails = $(this).serialize();
-      var userUrl = baseUrl + "/" + tripId + "/reserve";
-
-      $(".confirmation").text("You successfully reserved a spot on trip " + tripId + "!");
-
-      $.post(userUrl, userDetails, confirmationAlert);
-      $(".standout-trip").hide();
-      $('input[type="text"]').val('');
-    });
+    }
   });
 
 
